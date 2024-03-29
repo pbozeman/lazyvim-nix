a608a151cebc49ab8794b7e4e450db78ece830fe
Patrick Bozeman <pbozeman@gmail.com>
2024-03-29 15:51:45 -0700

    enable spell checking

--- config/lua/config/options.lua
+++ config/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.cmd("packadd cfilter")
+
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
