a4716aeb580633f1c6101481f32418f74471ecca
Patrick Bozeman <pbozeman@gmail.com>
2024-10-06 12:57:02 -0700

    restore tmux line movement work around

--- config/lua/config/keymaps.lua
+++ config/lua/config/keymaps.lua
@@ -5,3 +5,12 @@ vim.keymap.set("n", "gR", "<cmd> Trouble lsp_references <CR>")
 
 vim.keymap.set("n", "<tab>", "<cmd> bn <CR>")
 vim.keymap.set("n", "<S-tab>", "<cmd> bp <CR>")
+
+-- tmux spurious line movement work around
+-- https://github.com/LazyVim/LazyVim/discussions/658
+vim.keymap.set("n", "<A-k>", "<esc>k", { desc = "Move up" })
+vim.keymap.set("n", "<A-j>", "<esc>j", { desc = "Move down" })
+vim.keymap.set("i", "<A-k>", "<esc>gk", { desc = "Move up" })
+vim.keymap.set("i", "<A-j>", "<esc>gj", { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", "<esc>gk", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", "<esc>gj", { desc = "Move down" })
