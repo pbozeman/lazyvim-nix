288c492a6c50fbf5a311ae81c50faa18904c185f
Patrick Bozeman <pbozeman@gmail.com>
2025-03-18 13:58:18 -0700

    strip blank lines at end of comments in verilog

--- config/lua/config/autocmds.lua
+++ config/lua/config/autocmds.lua
@@ -5,6 +5,9 @@
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = { "*.v", "*.sv" },
   callback = function()
+    -- Strip trailing whitespace in the current buffer. Verilator doesn't do this for comments
+    vim.cmd([[%s/\s\+$//e]])
+
     local file = vim.fn.expand("<afile>")
     vim.cmd(
       '!verible-verilog-format --inplace --tryfromenv="indentation_spaces,column_limit,wrap_spaces,try_wrap_long_lines,assignment_statement_alignment,case_items_alignment,class_member_variable_alignment,distribution_items_alignment,enum_assignment_statement_alignment,formal_parameters_alignment,formal_parameters_indentation,module_net_variable_alignment,named_parameter_alignment,named_port_alignment,port_declarations_alignment,port_declarations_indentation,struct_union_members_alignment,wrap_end_else_clauses" '
