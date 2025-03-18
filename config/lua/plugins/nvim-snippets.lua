13a46627ee8980ab026e72f0db1aa159eab8e007
Patrick Bozeman <pbozeman@gmail.com>
2025-03-18 14:35:33 -0700

    disable snippets for verilog

---
+++ config/lua/plugins/nvim-snippets.lua
@@ -0,0 +1,8 @@
+return {
+  "garymjr/nvim-snippets",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  opts = {
+    friendly_snippets = true,
+    ignored_filetypes = { "verilog", "systemverilog" },
+  },
+}
